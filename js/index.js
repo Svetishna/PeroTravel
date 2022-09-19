96872da46d079182afeff4c6a64308d71d3b09fb
Svetishna <tockarenkos@yandex.ru>
2022-09-19 10:33:59 +0300

    slider

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
-import './modules/tabsNavigation.js'
+import './modules/tabsNavigation.js';
+import './modules/slider.js';
+import './modules/swiper-bundle.js';
+
 import { navChangeColor } from './modules/tabsNavigation.js';
 
 navChangeColor();
